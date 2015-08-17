9263247127062fa3e5b97dcd3bb7dd1b2dfddc64
akrisiungit <akrisiun@gmail.com>
2015-08-17 09:19:32 +0300

    less MonoMac

---
+++ fcmd/View.GTK/Backend/IGtk3.cs
@@ -0,0 +1,27 @@
+﻿using Xwt.Backends;
+
+namespace fcmd.View.GTK.Backend
+{
+    public interface IGtk3
+    {
+        IWidgetBackend Backend { get; }
+    }
+
+    public static class Gtk3Methods
+    {
+        public static Gtk.Widget GetNativeWidget(this Xwt.Widget widget)
+        {
+            var gtkWidget = Xwt.Toolkit.CurrentEngine.GetNativeWidget(widget) as Gtk.Widget;
+            return gtkWidget;
+        }
+
+        public static IWidgetBackend GetBackend(this Xwt.Widget w)
+        {
+            var end = Xwt.Toolkit.CurrentEngine.GetSafeBackend(w) as IWidgetBackend;
+            return end;
+            //if (w != null && w.BackendHost is XwtWidgetBackend)
+            //    return GetBackend((XwtWidgetBackend)w.Backend);
+            //return w != null ? w.Backend : null;
+        }
+    }
+}
